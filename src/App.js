20b2cd23fa820900096949407fa3c890c133421a
Efrain Lemus <efraintlemusm@gmail.com>
2022-06-24 00:27:59 -0600

    added api instead of hardcoded

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Cardlist from './Cardlist';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+// import { robots } from './robots';
 import './App.css';
 
 
@@ -11,9 +11,20 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            // robots: robots,
+            robots: [],
             searchField: ''
         }
+        // console.log('1');
+    }
+
+    componentDidMount() {
+        // console.log('check');
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }));
+        // this.setState({ robots: robots });
+        // console.log('2');
     }
 
     onSearchChange = (event) => {
@@ -26,6 +37,7 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
         })
+        // console.log('3');
         // console.log(filteredRobots);
         return (
             <div className="tc" >
